a53305df6ef5b9973cb37d83d6b09b44b54e2c18
Brandon Barksdale <bbark93@gmail.com>
2024-10-26 19:48:29 -0400

    wip

--- data/migrations/20241026144850_initial-migration.js
+++ data/migrations/20241026144850_initial-migration.js
@@ -14,10 +14,20 @@ exports.up = async function(knex) {
         table.string('ingredient_unit', 50)
     })
     .createTable('steps', table => {
-        table.increments()
+        table.increments('step_id')
+        table.string('steps_text', 200).notNullable()
+        table.integer('step_number').notNullable()
+        table.integer('recipe_id')
+            .unsigned()
+            .notNullable()
+            .references('recipes')
+            .inTable('recipes')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
     })
     .createTable('step_ingredients', table => {
         table.increments()
+            
     })
 };
 
